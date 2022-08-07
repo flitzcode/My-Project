348d87bed8c03a65549a70a0923cd31c17ad9872
muchamadfaiz <109614025+muchamadfaiz@users.noreply.github.com>
2022-08-07 11:37:02 +0700

    Add files via upload

---
+++ Dashboard Monitoring/main.py
@@ -0,0 +1,12 @@
+"""
+Aplikasi deteksi gempa terkini
+MODULARISASI DENGAN FUNCTION
+MODULARISASI DENGAN PACKAGES
+"""
+import gempaterkini
+
+if __name__ == '__main__':
+    gempa_di_indonesia = gempaterkini.GempaTerkini('https://bmkg.go.id')
+    print(f'Aplikasi utama menggunakan package yang memiliki deskripsi {gempa_di_indonesia.description}')
+    gempa_di_indonesia.tampilkan_keterangan()
+    gempa_di_indonesia.run()
